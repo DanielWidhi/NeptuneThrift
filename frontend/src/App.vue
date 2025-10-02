af08ba11140861818bc5bc80f3749294c2e981e6
Daniel Widhi <danielwidhi98@gmail.com>
2025-10-02 15:10:08 +0800

    update cdn font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,36 @@
 <script setup>
-import Footer from './components/Footer.vue'
-import Navbar from './components/Navbar.vue'
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
-  <h1 class="text-green-700">You did it!</h1>
+  <h1 class="text-green-700 title">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div>
+    <h1 class="title">Ini Judul dengan Font Montserrat</h1>
+    <p class="paragraph">Ini adalah paragraf biasa.</p>
+  </div>
   <Footer />
 </template>
 
 <style scoped>
+.title {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700; /* Bold */
+}
+
+.paragraph {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400; /* Regular */
+}
+.decorative-title {
+  font-family: "Bitcount Single Ink", cursive;
+  font-weight: 400; /* Atur ketebalan sesuai selera */
+  font-size: 2.5rem;
+  text-align: center;
+}
 </style>
