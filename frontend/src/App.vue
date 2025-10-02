cd3e01d227e3d36e3e02b2e90b156774dc1572e9
Daniel Widhi <danielwidhi98@gmail.com>
2025-10-02 13:09:28 +0800

    font kontol

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,14 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div class="p-8">
+    <h1 class="hey-comic text-4xl font-bold">NeptuneThrift</h1>
+    <p class="font-satoshi text-lg mt-2">
+      This text uses the Satoshi font. Ini adalah font kustom yang sudah kita impor.
+    </p>
+    <p class="mt-2">This text uses the default sans-serif font.</p>
+  </div>
 </template>
 
 <style scoped></style>
