1b7f62b1285d801fdeebd49ea718e23a8b7641c9
Daniel Widhi <danielwidhi98@gmail.com>
2025-10-02 13:17:22 +0800

    update app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
-<script setup></script>
+<script setup>
+import Navbar from "components/Navbar.vue";
+</script>
 
 <template>
+  <Navbar />
   <h1 class="text-green-700">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
-
-  <div class="p-8">
-    <h1 class="hey-comic text-4xl font-bold">NeptuneThrift</h1>
-    <p class="font-satoshi text-lg mt-2">
-      This text uses the Satoshi font. Ini adalah font kustom yang sudah kita impor.
-    </p>
-    <p class="mt-2">This text uses the default sans-serif font.</p>
-  </div>
 </template>
 
 <style scoped></style>
